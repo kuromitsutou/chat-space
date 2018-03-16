2babe708ae74ddde79b04d0cc94d55eafac39ff4
toshihide <get.enough.lactic.acid@gmail.com>
2018-03-17 08:14:59 +0900

    modify users routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'messages#index'
-  resource :user, only: [:edit, :update]
+  resource :users, only: [:edit, :update]
+  resource :groups, only: [:new, :create, :edit, :update]
 end
