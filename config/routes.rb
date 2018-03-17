9cb2dc7bc25ab4cf73986b4d69fd66924fba6b78
toshihide <get.enough.lactic.acid@gmail.com>
2018-03-17 17:29:38 +0900

    Modify user edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'messages#index'
-  resource :users, only: [:edit, :update]
+  resource :user, only: [:edit, :update]
   resource :groups, only: [:new, :create, :edit, :update]
 end
