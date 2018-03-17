c723152220bb039edf1292d40ea89b75e9ad8fb6
toshihide <get.enough.lactic.acid@gmail.com>
2018-03-17 15:56:10 +0000

    side_nav to partial

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,8 @@
 class GroupsController < ApplicationController
 
+  def index
+  end
+
   def new
     @group = Group.new
     @group.users << current_user
@@ -22,7 +25,7 @@ class GroupsController < ApplicationController
 
   private
   def group_params
-    params.require(:group).permit(:name, { :user_ids => [] })
+    params.require(:group).permit(:name, user_ids: [] )
   end
 
 end
