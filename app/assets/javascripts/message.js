1dd2f53104f27873f6137635ad860e24856e2331
toshihide <get.enough.lactic.acid@gmail.com>
2018-03-23 01:37:35 +0900

    add ajax

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,5 +2,15 @@ $(function(){
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     console.log(this);
+    var url = $(this).attr('action');
+    var formData = new FormData(this);
+    $.ajax({
+      url: url,
+      type: 'POST',
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
   });
 });
