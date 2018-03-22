9ee8d582828c0c139bd804602caa07f63bfe87e6
toshihide <get.enough.lactic.acid@gmail.com>
2018-03-23 01:46:29 +0900

    ajust view

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,4 +4,8 @@ class Message < ApplicationRecord
 
   validates :body, presence: true, unless: :image?
   mount_uploader  :image, ImageUploader
+
+  def format_posted_time
+    created_at.strftime("%Y年 %m月 %d日, %H時 %M分")
+  end
 end
